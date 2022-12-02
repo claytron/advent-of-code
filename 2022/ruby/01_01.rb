c291dc7e9535c910e6576dc042d62465a0410c41
Clayton Parker <me@claytron.com>
2022-12-01 16:41:30 -0800

    rubocop

--- 2022/ruby/01_01.rb
+++ 2022/ruby/01_01.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-totals = File.read('../01.input').split("\n\n").map {
-  |i| i.split.map(&:to_i)
-}.map(&:sum)
+totals = File.read('../01.input').split("\n\n").map do |i|
+  i.split.map(&:to_i)
+end.map(&:sum)
 
 puts totals.max
--- 2022/ruby/01_02.rb
+++ 2022/ruby/01_02.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-totals = File.read('../01.input').split("\n\n").map {
-  |i| i.split.map(&:to_i)
-}.map(&:sum)
+totals = File.read('../01.input').split("\n\n").map do |i|
+  i.split.map(&:to_i)
+end.map(&:sum)
 
 totals.sort!.reverse!
 puts totals[0..2].sum
