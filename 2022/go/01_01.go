71ef603bd0720e579f6853f43b7abfa99f9cdde6
Clayton Parker <me@claytron.com>
2022-12-04 23:22:20 -0500

    day 1 go

--- 2022/go/01_01.go
+++ 2022/go/01_01.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+   "fmt"
+   "os"
+   "strconv"
+   "strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+   file, _ := os.ReadFile("../01.input")
+
+   raw := strings.Split(string(file), "\n\n")
+   elves := []int{}
+
+   for _, raw_cals := range raw {
+      raw_cals_split := strings.Split(raw_cals, "\n")
+      total := 0
+
+      for _, cals_str := range raw_cals_split {
+         i, _ := strconv.Atoi(cals_str)
+         total += i
+      }
+      elves = append(elves, total)
+   }
+   fmt.Println(findMax(elves))
+}
+
+func findMax(a []int) (max int) {
+  max = a[0]
+  for _, value := range a {
+    if value > max {
+      max = value
+    }
+  }
+  return max
 }
