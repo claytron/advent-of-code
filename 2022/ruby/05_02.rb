aec957fad17ca5010e64f953929cf47bceaba865
Clayton Parker <me@claytron.com>
2022-12-06 14:53:59 -0500

    a better day 5 extraction

--- 2022/ruby/05_02.rb
+++ 2022/ruby/05_02.rb
@@ -2,25 +2,22 @@
 
 inputs = File.read('../05.input').split("\n\n")
 
-# This is a bit gnarly, but it gets rid of the brackets and preserves the stacks
-stacks_raw = inputs[0].gsub(/[\[\]]/, '').gsub('    ', '.').gsub(' ', '').split("\n")
+# Extract the data from the stack blob
+stacks_raw = inputs[0].split("\n").map do |row|
+  row.chars.each_slice(4).map do |container|
+    id = container[1]
+    id.strip.empty? ? nil : id
+  end
+end
 # get rid of the row with stack numbers
 stacks_raw.pop
 
-# Get the columns and transpose to have an array per stack
-stacks = stacks_raw.map(&:chars).transpose.map(&:reverse!).map do |stack|
-  Kernel.loop do
-    break unless stack.index('.')
-
-    # get rid of the placeholder dots in the structure
-    stack.pop
-  end
-  stack
-end
+# Transpose to have an array per stack and set the correct order and clean out empties
+stacks = stacks_raw.transpose.map(&:reverse!).map(&:compact)
 
 # Get the list of moves from the text
 moves = inputs[1].split("\n").map do |move|
-  move.sub('move ', '').sub(' from ', ',').sub(' to ', ',').split(',').map(&:to_i)
+  move.scan(/\d+/).map(&:to_i)
 end
 
 moves.each do |move|
