6a93dd20f2f48093fbf71625ce143be829b7a302
Clayton Parker <me@claytron.com>
2023-12-04 10:33:31 -0500

    simplify solution

--- 2023/ruby/03_01.rb
+++ 2023/ruby/03_01.rb
@@ -6,41 +6,24 @@ def valid?(pos, len, specs)
   !(Range.new(pos - 1, pos + len).to_a & specs).empty?
 end
 
-special_re = Regexp.new(/[^\w\d.]|_/)
-num_re = Regexp.new(/\d+/)
-specials_coords = []
-potential_coords = []
-
-inputs.each do |line|
-  specials_coords << line.enum_for(:scan, special_re).map { Regexp.last_match.begin(0) }
-  potential_coords << line.enum_for(:scan, num_re).map do
-    m = Regexp.last_match
-    "#{m.begin(0)}.#{m.to_s.length}.#{m}"
-  end
+def specials_pos(line)
+  special_re = Regexp.new(/[^\w\d.]|_/)
+  line.enum_for(:scan, special_re).map { Regexp.last_match.begin(0) }
 end
 
+num_re = Regexp.new(/\d+/)
 valid = []
-invalid = []
-potential_coords.each_with_index do |potentials, idx|
-  next if potentials.empty?
 
-  prev = idx - 1
-  nxt = idx + 1
+inputs.each_with_index do |line, idx|
   specials = []
-  prev_specials = specials_coords[prev]
-  nxt_specials = specials_coords[nxt]
+  specials.concat(specials_pos(inputs[idx - 1])) if idx.positive?
+  specials.concat(specials_pos(line))
+  specials.concat(specials_pos(inputs[idx + 1])) if inputs[idx + 1]
 
-  specials.concat(prev_specials) unless prev.negative?
-  specials.concat(specials_coords[idx])
-  specials.concat(nxt_specials) if nxt_specials
-
-  potentials.each do |potential|
-    pos, len, num = potential.split('.').map(&:to_i)
-    if valid?(pos, len, specials)
-    valid << num
-    else
-      invalid << num
-    end
+  line.enum_for(:scan, num_re).map do
+    m = Regexp.last_match
+    value = m.to_s
+    valid << value.to_i if valid?(m.begin(0), value.length, specials)
   end
 end
 
